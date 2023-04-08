0408680615792277173702b2e164bba7947975f2
Blessing Nyamajiwa <bynamajiwa00@gmail.com>
2023-04-08 15:49:00 +0200

    Bug fix with login function

--- hms/user-login.php
+++ hms/user-login.php
@@ -58,7 +58,7 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../index.html"><h2> HMS | Patient Login</h2></a>
+				<a href="../index.html"><h2> Health Informatics | Patient Login</h2></a>
 				</div>
 
 				<div class="box-login">
@@ -100,7 +100,7 @@ exit();
 					</form>
 
 					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> Health Informatics</span>. <span>All rights reserved</span>
 					</div>
 			
 				</div>
